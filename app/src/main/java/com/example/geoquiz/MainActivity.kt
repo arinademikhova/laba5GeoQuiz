cbbd450e4802701e7d73ccdda1104671efa0fd69
arina <arinademikhova@gmail.com>
2025-10-26 16:06:50 +0300

    addQuestion6

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -47,7 +47,8 @@ class MainActivity : ComponentActivity() {
                     Question("The Pacific Ocean is larger than the Atlantic Ocean.", true),
                     Question("The Suez Canal connects the Red Sea and the Indian Ocean.", false),
                     Question("The source of the Nile River is in Egypt.", false),
-                    Question("The Amazon River is the longest river in the Americas.", true)
+                    Question("The Amazon River is the longest river in the Americas.", true),
+                    Question("Lake Baikal is the world's oldest and deepest freshwater lake.", true)
                 )
                 GeoQuizScreen(questions)
             }
@@ -138,7 +139,8 @@ fun GeoQuizPreview() {
                 Question("The Pacific Ocean is larger than the Atlantic Ocean.", true),
                 Question("The Suez Canal connects the Red Sea and the Indian Ocean.", false),
                 Question("The source of the Nile River is in Egypt.", false),
-                Question("The Amazon River is the longest river in the Americas.", true)
+                Question("The Amazon River is the longest river in the Americas.", true),
+                Question("Lake Baikal is the world's oldest and deepest freshwater lake.", true)
             )
         )
     }
