33f43f92f7038300553338cd21b9e7f86eabc99c
arina <arinademikhova@gmail.com>
2025-10-26 16:06:17 +0300

    addQuestion5

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -46,7 +46,8 @@ class MainActivity : ComponentActivity() {
                     Question("Canberra is the capital of Australia.", true),
                     Question("The Pacific Ocean is larger than the Atlantic Ocean.", true),
                     Question("The Suez Canal connects the Red Sea and the Indian Ocean.", false),
-                    Question("The source of the Nile River is in Egypt.", false)
+                    Question("The source of the Nile River is in Egypt.", false),
+                    Question("The Amazon River is the longest river in the Americas.", true)
                 )
                 GeoQuizScreen(questions)
             }
@@ -136,7 +137,8 @@ fun GeoQuizPreview() {
                 Question("Canberra is the capital of Australia.", true),
                 Question("The Pacific Ocean is larger than the Atlantic Ocean.", true),
                 Question("The Suez Canal connects the Red Sea and the Indian Ocean.", false),
-                Question("The source of the Nile River is in Egypt.", false)
+                Question("The source of the Nile River is in Egypt.", false),
+                Question("The Amazon River is the longest river in the Americas.", true)
             )
         )
     }
